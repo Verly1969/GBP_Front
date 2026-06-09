80545406db91fca5357aee0d9e358b132352e619
Verly1969 <eddyverly@icloud.com>
2026-06-09 11:22:22 +0200

    feat: update password form with visibility toggle and validation error handling; refine profile modification title

--- src/app/features/pages/password/password.ts
+++ src/app/features/pages/password/password.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password',
@@ -8,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class Password {
 
+  private readonly fb = inject(FormBuilder);
+  private readonly authService = inject(AuthService);
+
+  showPassword = signal(false);
+  isLoading    = signal(false);
+
+  form = this.fb.group({
+    oldPassword:     ['', Validators.required],
+    newPassword:     ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  })
+
+// ===========================================================
+// Methodes déléguées de AuthService 
+// ===========================================================
+  togglePassword(): void { this.authService.toggleVisibilityPassword(this.showPassword); }
+
+  get firstnameErrors(): string | null { return this.authService.getRequiredErrors(this.form.get('firstname'), 'Le prénom'); }
+  get lastnameErrors():  string | null { return this.authService.getRequiredErrors(this.form.get('lastname'), "Le nom de famille"); } 
+  get passwordErrors() : string | null { return this.authService.getPasswordErrors(this.form.get('password')); }
+  get confirmPasswordErrors(): string | null {
+    return this.authService.getConfirmPasswordErrors(
+      this.form.get('confirmPassword'),
+      this.form
+    )
+  }  
 }
